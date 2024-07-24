b7264495b300d404e19e7f9c6b2555c85e1e985f
Jonny <Gates>
2024-07-24 21:07:22 +0100

    Install tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/img/hero-pattern.svg')",
+      },
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#FF7C5B",
+
+          secondary: "#599C9E",
+
+          accent: "#473849",
+
+          neutral: "#FCFFFD",
+
+          "base-100": "#FCFFFD",
+
+          info: "#473849",
+
+          success: "#D8F793",
+
+          warning: "#E3C567",
+
+          error: "#F8333C",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
