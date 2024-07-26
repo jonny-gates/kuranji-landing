55a27c6a52ab3ac79eae417a848ed97dbf475551
Jonny <Gates>
2024-07-26 09:28:56 +0100

    Add klaviyo popout

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-
+document
+  .getElementById("founders-club-form")
+  .addEventListener("click", function () {
+    window._klOnsite = window._klOnsite || [];
+    window._klOnsite.push(["openForm", "VBSrFg"]);
+  });
